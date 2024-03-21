aa3cd99ecf657fb35089641a06b33cf813d9ed73
JackXLin <jack.llll@yahoo.co.uk>
2024-03-21 11:46:52 +0000

    Third commit, added mongo_insert() method to populate the collection.

--- swapi_oop.py
+++ swapi_oop.py
@@ -34,24 +34,11 @@ class Starwars:
                 updated_pilots.append(pilot)
             ship["pilots"] = updated_pilots
 
+    def mongo_insert(self):
+        for ship in self.starships["results"]:
+            self.db.starships.insert_one(ship)
 
-
-
-        # import pymongo
-        # from swapi import *
-        # client = pymongo.MongoClient()
-        #
-        # db = client["starwars"]
-        #
-        # for ship in starships["results"]:
-        #     updated_pilots = []
-        #     for pilot in ship["pilots"]:
-        #         star_name = get_pilots(pilot)["name"]
-        #         star_id = db.characters.find_one({"name": star_name}, {"_id": 1})
-        #         pilot = star_id["_id"]
-        #         updated_pilots.append(pilot)
-        #     ship["pilots"] = updated_pilots
 
 class1 = Starwars()
 class1.get_pilots_id()
-print(class1.starships)
+class1.mongo_insert()
