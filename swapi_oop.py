f47cbd5c7c738379bf0b725d058ce6790d108f90
JackXLin <jack.llll@yahoo.co.uk>
2024-03-21 11:37:19 +0000

    Second commit, added get_pilots_id method to replace pilot urls with ObjectId from MongoDB

--- swapi_oop.py
+++ swapi_oop.py
@@ -11,7 +11,7 @@ class Starwars:
         self.starships = None
         try:
             self.response = requests.get(self.url_starship)
-            starships = self.response.json()
+            self.starships = self.response.json()
         except:
             print("invalid requests")
 
@@ -24,19 +24,34 @@ class Starwars:
         except:
             print("pilots don't exists!")
 
-# starships = None
-# url_starship = "https://swapi.dev/api/starships"
-# try:
-#     response = requests.get(url_starship)
-#     starships = response.json()
-# except:
-#     print("invalid requests")
+    def get_pilots_id(self):
+        for ship in self.starships["results"]:
+            updated_pilots = []
+            for pilot in ship["pilots"]:
+                star_name = self.get_pilots(pilot)["name"]
+                star_id = self.db.characters.find_one({"name": star_name}, {"_id": 1})
+                pilot = star_id["_id"]
+                updated_pilots.append(pilot)
+            ship["pilots"] = updated_pilots
 
-# def get_pilots(pilot_url):
-#     try:
-#         if pilot_url:
-#             pilot_response = requests.get(pilot_url)
-#             pilot_results = pilot_response.json()
-#             return pilot_results
-#     except:
-#         print("pilots don't exists!")
+
+
+
+        # import pymongo
+        # from swapi import *
+        # client = pymongo.MongoClient()
+        #
+        # db = client["starwars"]
+        #
+        # for ship in starships["results"]:
+        #     updated_pilots = []
+        #     for pilot in ship["pilots"]:
+        #         star_name = get_pilots(pilot)["name"]
+        #         star_id = db.characters.find_one({"name": star_name}, {"_id": 1})
+        #         pilot = star_id["_id"]
+        #         updated_pilots.append(pilot)
+        #     ship["pilots"] = updated_pilots
+
+class1 = Starwars()
+class1.get_pilots_id()
+print(class1.starships)
